30576562e58959c69e2b4845431c5839e74e68d9
mine <viniciusroriz21@gmail.com>
2022-07-04 21:24:43 -0300

    ex e

--- lista_1/e.c
+++ lista_1/e.c
@@ -7,31 +7,18 @@
 
 int count = 0;
 
-void handle_sig(int sig) {
-    pid_t pid;
-
-    if(count++ == 0){
-        if((pid = fork()) == 0){
-            exit(0);
-        }else{
-            pause();
-        }
-    }else if(count++ == 1){
-        if(pid > 0){
-            waitpid(-1, NULL, WNOHANG);
-        }
-    }else{
-        exit(0);
-    }
-}
+void handle_sig(int sig) {}
 
 int main() {
     signal(SIGUSR1, handle_sig); // 10
     signal(SIGUSR2, handle_sig); // Geral
     
-    while(1){
-        pause();
-    }
+    pause();
+    if(fork() == 0)
+        exit(0);
+    pause();
+    wait(NULL);
+    pause();
     
     return 0;
 }
