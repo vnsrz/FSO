6292e36e2c6c6ddd362db173e10ff9cf1b2d7b7d
mine <viniciusroriz21@gmail.com>
2022-07-09 19:19:32 -0300

    update

--- testes.c
+++ testes.c
@@ -5,14 +5,14 @@
 #include <sys/types.h>
 #include <signal.h>
 
-int main(void) {
-  char *binaryPath = "/bin/ls";
-  char *arg1 = "-a";
-  char *arg2 = "/home/mine";
+int testaExec(void) {
+  	char *binaryPath = "/bin/ls";
+  	char *arg1 = "-a";
+  	char *arg2 = "/home/mine";
  
-  execl(binaryPath, binaryPath, arg1, arg2, NULL);
+  	execl(binaryPath, binaryPath, arg1, arg2, NULL);
  
-  return 0;
+  	return 0;
 }
 
 int paiEFilho(){
@@ -22,6 +22,54 @@ int paiEFilho(){
     a=fork();
 
     printf("sou #%d e meu pai é #%d\n", getpid(), getppid());
+}
+
+pid_t c;
+
+void seg(void){
+	sleep(20);
+	exit(0);
+}
+
+void sig_handler(int s){
+	printf("sou %d e recebi %d\n", getpid(), s);
+	kill(c, SIGUSR2);
+}
+
+int main(void){
+    pid_t a, b, d, e, f, g;
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
+    
+	a = getpid();
+
+	b = fork();
+    if(b == 0) // estou no processo b
+		seg();
+	
+	c=fork();
+	if(c == 0){ // estou no processo c
+		e = fork();
+		if(e==0)	// estou no processo e
+			seg();
+		seg();
+	}
+
+	d = fork();
+	if(d==0){ // estou no processo d
+		f = fork();
+		if(f == 0) // estou no processo f
+			seg();
+		g = fork();
+		if(g == 0){ // estou no processo g
+			sleep(10);
+			kill(a, SIGUSR1);
+			seg();
+		} 
+	}
+		
+	seg();
+	
 }
 
 // p = 603
